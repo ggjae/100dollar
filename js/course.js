b8d8ec209c3c975a0ac63b834261789f7cc6ba90
ggjae_ <ggjae_@daum.net>
2019-12-20 00:42:55 +0900

    add

--- js/course.js
+++ js/course.js
@@ -7,6 +7,11 @@ const changeContent = function(event){
 	document.querySelector(target).classList.add("selected");
 	document.querySelector(target).classList.remove("deselected");
 };
+jQuery(document).ready(function($){
+    setTimeout(function(){
+        $('.trans--grow').addClass('grow');
+    }, 275);
+});
 
 const make_PBLObjective = function(){
 	const pbl_objective = document.querySelector("#pbl_objective");
@@ -84,3 +89,43 @@ document.querySelectorAll(".dropdown-list").forEach((element)=>{
 })
 
 document.querySelector("#pbl_groupBtn").addEventListener('click', fold_pbl_group);
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight;
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+window.onload = hasScrolled;
